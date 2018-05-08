983f42a03019596d7f44deacff3e2888a7cf3273
baopingli <l>
2018-05-08 17:14:42 +0800

    fsdfsd

---
+++ MyWindowsService/MyService1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWindowsService
+{
+    public partial class MyService1 : ServiceBase
+    {
+        public MyService1()
+        {
+            InitializeComponent();
+        }
+
+        string filePath= @"D:\MyServiceLog.txt";
+
+        protected override void OnStart(string[] args)
+        {
+            //写入一个服务启动
+            using (FileStream stream = new FileStream(filePath, FileMode.Append))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine($"{DateTime.Now},服务启动！");
+            }
+
+        }
+
+        protected override void OnStop()
+        {
+            //写入了一个服务停止
+            using (FileStream stream = new FileStream(filePath, FileMode.Append))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine($"{DateTime.Now},服务停止！");
+            }
+
+        }
+    }
+}
---
+++ WindowsServiceClient/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Configuration.Install;
+using System.Collections;
+
+namespace WindowsServiceClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string serviceFilePath = $"{Application.StartupPath}\\MyWindowsService.exe";
+        string serviceName = "MyService";
+        //事件：安装服务
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.IsServiceExisted(serviceName)) this.UninstallService(serviceName);
+            this.InstallService(serviceFilePath);
+
+        }
+
+
+        //卸载服务
+        private void UninstallService(string serviceName)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                installer.Uninstall(null);
+            }
+
+            //throw new NotImplementedException();
+        }
+        //安装服务
+        private void InstallService(string serviceFilePath)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                IDictionary savedState = new Hashtable();
+                installer.Install(savedState);
+                installer.Commit(savedState);
+
+            }
+            //throw new NotImplementedException();
+        }
+        //判断服务是否存在
+        private bool IsServiceExisted(string serviceName)
+        {
+
+            ServiceController[] services = ServiceController.GetServices();
+            foreach(ServiceController sc in services)
+            {
+                if(sc.ServiceName.ToLower()==serviceName.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+        //启动服务
+        private void ServiceStart(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if(control.Status==ServiceControllerStatus.Stopped)
+                {
+                    control.Start();
+                }
+            }
+        }
+
+        //停止服务
+        private void ServiceStop(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if(control.Status==ServiceControllerStatus.Running)
+                {
+                    control.Stop();
+                }
+            }
+        }
+        //事件：启动服务
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName)) this.ServiceStart(serviceName);
+
+        }
+
+        //事件：停止服务
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName)) this.ServiceStop(serviceName);
+
+        }
+        //事件：卸载服务
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(this.IsServiceExisted(serviceName))
+            {
+                this.ServiceStop(serviceName);
+                this.UninstallService(serviceFilePath);
+            }
+
+        }
+    }
+}
